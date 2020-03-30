50e87e261a2141e0358a32e41814f0ae87d066b2
joaopat98 <joaopat98@gmail.com>
2020-03-30 19:30:14 +0100

    Add player view gizmo

---
+++ Assets/Editor/Player/PlayerView.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Player))]
+public class PlayerView : Editor
+{
+    private static Vector3 Rotate(Vector3 point, Vector3 pivot, float angle)
+    {
+        var dir = point - pivot;
+        dir = Quaternion.Euler(0, angle, 0) * dir;
+        point = dir + pivot;
+        return point;
+    }
+
+    [DrawGizmo(GizmoType.Selected)]
+    static void DrawGizmos(Player player, GizmoType gizmoType)
+    {
+        Vector3 origin = player.transform.position;
+        Vector3 front = origin + (player.transform.forward * player.ViewRange);
+        Gizmos.DrawLine(origin, Rotate(front, origin, player.ViewAngle));
+        Gizmos.DrawLine(origin, Rotate(front, origin, -player.ViewAngle));
+        for (float angle = 0; angle < player.ViewAngle; angle += 0.5f)
+        {
+            Gizmos.DrawLine(Rotate(front, origin, angle), Rotate(front, origin, angle + 0.5f));
+            Gizmos.DrawLine(Rotate(front, origin, -angle), Rotate(front, origin, -angle - 0.5f));
+        }
+    }
+}
+
