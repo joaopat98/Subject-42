e7cb84e1669304abae9e49e46f8a5d7bde7ea4ca
dgpereira98 <dgpereira98@gmail.com>
2020-04-12 15:16:52 +0100

    first draft

---
+++ Assets/PickupCollectible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupCollectible : MonoBehaviour
+{
+
+    public Text Name;
+    public Text Description;
+    public string NameContent;
+    public string DescriptionContent;
+
+    void OnTriggerStay(Collider collider)
+    {   
+        if(collider.tag == "Player" && Input.GetKeyDown(KeyCode.Z))
+        {
+            Name.text = NameContent;
+            Description.text = DescriptionContent;
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/VisibilityPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisibilityPanel : MonoBehaviour
+{
+    private bool show = false;
+    public GameObject Panel;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            show = !show;
+        }
+
+        Panel.gameObject.SetActive(show);
+    }
+}
