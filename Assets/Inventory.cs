d79f0a74f9312b55e005e97dcc50844b691ddf7e
dgpereira98 <dgpereira98@gmail.com>
2020-04-18 01:08:27 +0100

    Collectible (without xbox controls)

---
+++ Assets/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{   
+    public GameObject[] slots;
+    private bool show = false;
+    public GameObject Canvas;
+    public GameObject Select;
+    public Text Name;
+    public Text Description;
+    public int current = 0;
+    public List<string> Names = new List<string>();
+    public List<string> Descriptions = new List<string>();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V)){show = !show;}
+        
+                if(show && Names.Count > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.K)){
+                if(current > 0){
+                    current--;
+                }
+            }
+
+            else if (Input.GetKeyDown(KeyCode.L)){
+                if(current <  Names.Count-1){
+                    current++;
+                }
+            }
+
+            Name.text = Names[current];
+            Description.text = Descriptions[current];
+            Select.transform.position = slots[current].transform.position;
+        }
+
+        else
+        {
+            Name.text = "Empty";
+            Description.text = "When you collect an item you'll see its description here.";
+        }       
+
+        Canvas.gameObject.SetActive(show);
+    }
+}
--- Assets/PickupCollectible.cs
+++ Assets/PickupCollectible.cs
@@ -5,19 +5,25 @@ using UnityEngine.UI;
 
 public class PickupCollectible : MonoBehaviour
 {
-
-    public Text Name;
-    public Text Description;
+    private Inventory inventory;
+    public GameObject itemButton;
     public string NameContent;
     [TextArea(3, 10)]
     public string DescriptionContent;
+
+    public void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
 
     void OnTriggerStay(Collider collider)
     {
         if (collider.tag == "Player" && Input.GetKeyDown(KeyCode.Z))
         {
-            Name.text = NameContent;
-            Description.text = DescriptionContent;
+            inventory.Names.Add(NameContent);
+            inventory.Descriptions.Add(DescriptionContent);
+            inventory.current = inventory.Names.Count-1;
+            Instantiate(itemButton, inventory.slots[inventory.current].transform, false);
             Destroy(gameObject);
         }
     }
