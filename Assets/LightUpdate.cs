62e4a3c1e3badda72c5c2c73b49d5c6f35eb8f38
joaopat98 <joaopat98@gmail.com>
2020-05-11 10:49:45 +0100

    Guard Spotlights

---
+++ Assets/LightUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightUpdate : MonoBehaviour
+{
+    Guard guard;
+    Light spotlight;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        guard = GetComponentInParent<Guard>();
+        spotlight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spotlight.range = guard.ViewRange;
+        spotlight.innerSpotAngle = 0;
+        spotlight.spotAngle = 2 * guard.ViewAngle;
+    }
+}
