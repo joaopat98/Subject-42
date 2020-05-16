f2e3a4545719198b5abb9e85068a0c40782af4c8
Victor Ribeiro <victor.portusil0@gmail.com>
2020-05-16 22:07:09 +0100

    Level Transitions Added.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator Transition;
+
+    public float TransitionTime = 1.5f;
+    
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        Transition.SetTrigger("Start");
+        yield return new WaitForSeconds(TransitionTime);
+        SceneManager.LoadScene(LevelIndex);
+    }
+}
---
+++ Assets/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    public LevelLoader LevelLoader;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            LevelLoader.LoadNextLevel();
+        }
+    }
+}
