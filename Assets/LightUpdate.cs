87e41e6ba45f2010a5d02238a80f1cdb7ec565ed
joaopat98 <joaopat98@gmail.com>
2020-05-18 00:22:06 +0100

    added lighs to first level, fix some bugs

--- Assets/LightUpdate.cs
+++ Assets/LightUpdate.cs
@@ -8,71 +8,19 @@ public class LightUpdate : MonoBehaviour
     Guard guard;
     Light spotlight;
     VLight volume;
-    public float Falloff = 5;
-    [Range(0, Mathf.Infinity)]
-    public float ColorFadeTime = 0.25f;
-    private GuardActionType currentState;
-    private Coroutine currentFade;
     // Start is called before the first frame update
     void OnEnable()
     {
         guard = GetComponentInParent<Guard>();
         spotlight = GetComponent<Light>();
         volume = GetComponent<VLight>();
-        currentState = GuardActionType.Patrol;
-        spotlight.color = guard.PatrolColor;
-        volume.colorTint = guard.PatrolColor;
+        volume.colorTint = spotlight.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (guard.action != null && currentState != guard.action.type)
-        {
-            currentState = guard.action.type;
-            if (currentFade != null)
-                StopCoroutine(currentFade);
-            currentFade = StartCoroutine(FadeToColor());
-        }
-        spotlight.range = guard.ViewRange;
-        spotlight.innerSpotAngle = 2 * guard.ViewAngle;
-        spotlight.spotAngle = 2 * guard.ViewAngle + Falloff;
-        volume.spotRange = guard.ViewRange;
-        volume.spotAngle = 2 * guard.ViewAngle;
-    }
-
-    IEnumerator FadeToColor()
-    {
-        var prevColor = spotlight.color;
-        Color toColor;
-        switch (currentState)
-        {
-            case GuardActionType.Patrol:
-                toColor = guard.PatrolColor;
-                break;
-            case GuardActionType.Check:
-                toColor = guard.CheckColor;
-                break;
-            case GuardActionType.Chase:
-                toColor = guard.ChaseColor;
-                break;
-            default:
-                Debug.LogError("Color for " + currentState.ToString() + " not defined!");
-                toColor = Color.black;
-                break;
-        }
-        float t = 0;
-        while (t < ColorFadeTime)
-        {
-            float fac = EasingFunction.EaseInOutCubic(0, 1, t / ColorFadeTime);
-            Color newColor = Color.Lerp(prevColor, toColor, fac);
-            spotlight.color = newColor;
-            volume.colorTint = newColor;
-            yield return 0;
-            t += Time.deltaTime;
-        }
-        spotlight.color = toColor;
-        volume.colorTint = toColor;
-        currentFade = null;
+        volume.spotRange = spotlight.range;
+        volume.spotAngle = spotlight.innerSpotAngle;
     }
 }
