1b3c295b2ca16762c2614df6a5eae473f6e56cc8
Adrian <adrianrodriguesm98@gmail.com>
2020-05-22 04:40:21 +0100

    NewBulletPrefab added

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,8 +6,10 @@ public class Bullet : MonoBehaviour
 {
     public float Speed = 5;
     public float Range = 20;
+    public Color color;
     private float travelled = 0;
     private Vector3 prevPos;
+
 
     void OnTriggerEnter(Collider col)
     {
@@ -29,6 +31,21 @@ public class Bullet : MonoBehaviour
     {
         GetComponent<Rigidbody>().velocity = transform.forward * Speed;
         prevPos = transform.position;
+        GlowBullet();
+
+    }
+
+    void GlowBullet()
+    {
+        foreach (var mat in GetComponentInChildren<MeshRenderer>().materials)
+        {
+            if (mat.HasProperty("GlowColor") && mat.HasProperty("GlowIntensity"))
+            {
+                mat.SetFloat("GlowIntensity", 1);
+                mat.SetColor("GlowColor", color);
+            }
+
+        }
     }
 
     // Update is called once per frame
